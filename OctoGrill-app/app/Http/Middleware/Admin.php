e511abad5741e455a45d0d88fdc4e7f3042113a6
Yeto2 <bota.tll2004@gmail.com>
2023-04-19 17:42:40 +0000

    setup applications roles

--- OctoGrill-app/app/Http/Middleware/Admin.php
+++ OctoGrill-app/app/Http/Middleware/Admin.php
@@ -16,7 +16,7 @@ class Admin
      */
     public function handle(Request $request, Closure $next)
     {
-        if (!auth()->check() || !auth()->user()->is_admin) {
+        if (!auth()->check() || !auth()->user()->role == 'admin') {
             abort(403);
         }
         return $next($request);
